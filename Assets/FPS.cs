e610a6e4235fe762887c4d7cbd90f990ec7d91f5
Mikael Zackrisson <micke.zackrisson@gmail.com>
2014-10-02 09:32:02 +0200

    removed sample assets

---
+++ Assets/FPS.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
+
+public class FPS : MonoBehaviour 
+{
+	public GUIText fpsCounter;
+	
+	public int sampleFrames = 30;
+	
+	public int greenFrames = 60;
+	public int yellowFrames = 40;
+	
+	List<float> frameTimes = new List<float>();
+	
+	private float fps;
+	
+	void Update () 
+	{
+		frameTimes.Add(Time.deltaTime);
+		
+		if (frameTimes.Count() == (sampleFrames -1))
+		{
+			DisplayAverageFPS();
+			
+			ChangeFrameColours();
+			
+			frameTimes.Clear();
+		}
+	}
+	
+	void DisplayAverageFPS()
+	{
+		fps = 1 / frameTimes.Average();
+		
+		fpsCounter.text = fps.ToString("F2");
+	}
+	
+	void ChangeFrameColours()
+	{
+		if (fps >= greenFrames)
+			fpsCounter.color = Color.green;
+		else if (fps >= yellowFrames)
+			fpsCounter.color = Color.yellow;
+		else
+			fpsCounter.color = Color.red;
+	}
+}
--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -3,6 +3,115 @@
 public class MenuScript : MonoBehaviour
 {
 	private GUISkin skin;
+	private string[] menuOptions;
+	private int selectedIndex;
+	
+	void Start()
+	{
+		// Load a skin for the buttons
+		skin = Resources.Load("GUIskin") as GUISkin;
+
+		menuOptions = new string[4];
+		menuOptions[0] = "Start";
+		menuOptions[1] = "Settings";
+		menuOptions[2] = "About";
+		menuOptions[3] = "Exit";
+	
+		selectedIndex = 0;
+	}
+
+	// Function to scroll through possible menu items array, looping back to start/end depending on direction of movement.
+	int menuSelection (string[] menuItems, int selectedItem, string direction)
+	{
+		if (direction == "up") {
+			if (selectedItem == 0) {
+				selectedItem = menuItems.Length - 1;
+			} else {
+				selectedItem -= 1;
+			}
+		}
+		
+		if (direction == "down") {
+			if (selectedItem == menuItems.Length - 1) {
+				selectedItem = 0;
+			} else {
+				selectedItem += 1;
+			}
+		}
+		
+		return selectedItem;
+	}
+
+	void Update ()
+	{
+		if(Input.GetKeyDown("down")) {
+			selectedIndex = menuSelection(menuOptions, selectedIndex, "down");
+		}
+		
+		if(Input.GetKeyDown("up")) {
+			selectedIndex = menuSelection(menuOptions, selectedIndex, "up");
+		}
+	}
+
+	void OnGUI ()
+	{
+		const int buttonWidth = 420;
+		const int buttonHeight = 80;
+		const int buttonMargin = 35;
+		int startHeight = (int)(Screen.height * 0.4f);
+		int startWidth = (int)(Screen.width * 0.2f);
+		
+		// Set the skin to use
+		GUI.skin = skin;
+		
+		// Draw a button to start the game
+		GUI.SetNextControlName ("Start");
+		if (GUI.Button(
+			new Rect(startWidth - buttonWidth/2, startHeight, buttonWidth, buttonHeight),
+			"START"
+			))
+		{
+			// On Click, load the first level.
+			Application.LoadLevel("level1"); // "Stage1" is the scene name
+		}
+
+		GUI.SetNextControlName ("Settings");
+		if (GUI.Button(
+			new Rect(startWidth - buttonWidth/2, startHeight + buttonHeight + buttonMargin, buttonWidth, buttonHeight),
+			"SETTINGS"
+			))
+		{
+
+		}
+
+		GUI.SetNextControlName ("About");
+		if (GUI.Button(
+			new Rect(startWidth - buttonWidth/2, startHeight + 2*(buttonHeight + buttonMargin), buttonWidth, buttonHeight),
+			"ABOUT"
+			))
+		{
+
+		}
+
+		GUI.SetNextControlName ("Exit");
+		if (GUI.Button(
+			new Rect(startWidth - buttonWidth/2, startHeight + 3*(buttonHeight + buttonMargin), buttonWidth, buttonHeight),
+			"Exit"
+			))
+		{
+			
+		}
+
+		
+		GUI.FocusControl (menuOptions[selectedIndex]);
+	}
+}
+
+/*using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+	private GUISkin skin;
 	
 	void Start()
 	{
@@ -48,3 +157,4 @@ public class MenuScript : MonoBehaviour
 		}
 	}
 }
+*/
